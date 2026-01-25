7ca885b0078e830b0331450404c4926b261271cc
shivanshgupta612 <60267973+shivanshgupta612@users.noreply.github.com>
2026-01-25 11:51:44 +0530

    Musafir initial commit

---
+++ src/main/java/com/musafir/MusafirApplication.java
@@ -0,0 +1,13 @@
+package com.musafir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusafirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusafirApplication.class, args);
+	}
+
+}
